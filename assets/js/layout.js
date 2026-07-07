9cbd577e7d63bcb0309740df96a5a96cdf61030a
Geoffrey Duval <geoffrey-duval@wanadoo.fr>
2026-07-07 23:01:57 +0200

    correction bug affichage menu sur téléphone portable

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,39 +1,41 @@
 async function loadNav() {
+  const response = await fetch("/includes/nav.html");
+  const html = await response.text();
 
-    const response = await fetch("/includes/nav.html");
-    const html = await response.text();
-
-    document.getElementById("nav-container").innerHTML = html;
+  document.getElementById("nav-container").innerHTML = html;
 
 }
 
 async function loadFooter() {
+  const response = await fetch("/includes/footer.html");
+  const html = await response.text();
+  document.getElementById("footer-container").innerHTML = html;
 
-     const response = await fetch("/includes/footer.html");
-    const html = await response.text();
-    document.getElementById("footer-container").innerHTML = html;
+}
+
+function initializePage() {
+    const nav = document.getElementById("nav");
+    const burger = document.getElementById("navBurger");
+
+    if (!nav || !burger) {
+        console.log("Menu mobile non initialisé");
+        return;
+    }
+
+    burger.addEventListener("click", () => {
+
+        nav.classList.toggle("open");
+    });
+
 
 }
 
-async function initializePage() {
-document.addEventListener("DOMContentLoaded", () => {
-
-  const btn = document.getElementById("navToggle");
-  const nav = document.getElementById("nav");
-
-  btn.addEventListener("click", () => {
-    nav.classList.toggle("open");
-  });
-
-});
-
-}
 
 async function initLayout() {
 
-    await loadNav();
-    await loadFooter();
+  await loadNav();
+  await loadFooter();
 
-    initializePage();
+  initializePage();
 
 }
